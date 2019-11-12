3bb2552092b1dfefc7e72891a861ab7b3b3b3b52
Koen78 <32360820+Koen78@users.noreply.github.com>
2019-11-12 14:37:13 +0100

    Merge branch 'jhipster-entities-book-5178496270945950'
    # Conflicts:
    #	.jhipster/Boek.json
    #	.jhipster/Persoon.json
    #	src/main/java/io/github/jhipster/application/domain/Boek.java
    #	src/main/java/io/github/jhipster/application/domain/Persoon.java
    #	src/main/java/io/github/jhipster/application/repository/BoekRepository.java
    #	src/main/java/io/github/jhipster/application/repository/PersoonRepository.java
    #	src/main/java/io/github/jhipster/application/repository/search/BoekSearchRepository.java
    #	src/main/java/io/github/jhipster/application/repository/search/PersoonSearchRepository.java
    #	src/main/java/io/github/jhipster/application/service/dto/BoekDTO.java
    #	src/main/java/io/github/jhipster/application/service/dto/PersoonDTO.java
    #	src/main/java/io/github/jhipster/application/web/rest/BoekResource.java
    #	src/main/java/io/github/jhipster/application/web/rest/PersoonResource.java
    #	src/main/resources/config/liquibase/master.xml
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix-delete-dialog.component.ts
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix-update.component.html
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix-update.component.ts
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix.component.html
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix.component.ts
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix.module.ts
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix.route.ts
    #	src/main/webapp/app/entities/boek-my-suffix/boek-my-suffix.service.ts
    #	src/main/webapp/app/entities/entity.module.ts
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix-delete-dialog.component.ts
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix-update.component.html
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix-update.component.ts
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix.component.html
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix.component.ts
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix.module.ts
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix.route.ts
    #	src/main/webapp/app/entities/persoon-my-suffix/persoon-my-suffix.service.ts
    #	src/test/java/io/github/jhipster/application/web/rest/BoekResourceIT.java
    #	src/test/java/io/github/jhipster/application/web/rest/PersoonResourceIT.java
    #	src/test/javascript/e2e/entities/boek-my-suffix/boek-my-suffix.page-object.ts
    #	src/test/javascript/e2e/entities/boek-my-suffix/boek-my-suffix.spec.ts
    #	src/test/javascript/e2e/entities/persoon-my-suffix/persoon-my-suffix.page-object.ts
    #	src/test/javascript/e2e/entities/persoon-my-suffix/persoon-my-suffix.spec.ts
    #	src/test/javascript/spec/app/entities/boek-my-suffix/boek-my-suffix-delete-dialog.component.spec.ts
    #	src/test/javascript/spec/app/entities/boek-my-suffix/boek-my-suffix-detail.component.spec.ts
    #	src/test/javascript/spec/app/entities/boek-my-suffix/boek-my-suffix-update.component.spec.ts
    #	src/test/javascript/spec/app/entities/boek-my-suffix/boek-my-suffix.component.spec.ts
    #	src/test/javascript/spec/app/entities/boek-my-suffix/boek-my-suffix.service.spec.ts
    #	src/test/javascript/spec/app/entities/persoon-my-suffix/persoon-my-suffix-delete-dialog.component.spec.ts
    #	src/test/javascript/spec/app/entities/persoon-my-suffix/persoon-my-suffix-detail.component.spec.ts
    #	src/test/javascript/spec/app/entities/persoon-my-suffix/persoon-my-suffix-update.component.spec.ts
    #	src/test/javascript/spec/app/entities/persoon-my-suffix/persoon-my-suffix.component.spec.ts
    #	src/test/javascript/spec/app/entities/persoon-my-suffix/persoon-my-suffix.service.spec.ts

---
+++ src/main/java/be/koen/voorraad/domain/Winkel.java
@@ -0,0 +1,89 @@
+package be.koen.voorraad.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.github.jhipster.application.domain.AbstractAuditingEntity;
+import io.github.jhipster.application.domain.Authority;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * winkel.
+ */
+@Entity
+@Table(name = "kbe_winkel")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@org.springframework.data.elasticsearch.annotations.Document(indexName = "winkel")
+public class Winkel extends AbstractAuditingEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "omschrijving", length = 50, unique = true, nullable = false)
+    private String omschrijving;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+        name = "jhi_user_authority",
+        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+        inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")})
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    @BatchSize(size = 20)
+    private Set<Authority> authorities = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+
+    public void setOmschrijving(String omschrijving) {
+        this.omschrijving = omschrijving;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Winkel)) {
+            return false;
+        }
+        return id != null && id.equals(((Winkel) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return "Winkel{" +
+            "id='" + id + '\'' +
+            ", omschrijving='" + omschrijving + '\''  +
+            "}";
+    }
+}
